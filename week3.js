ffdfcd6d83c8c8ab977d546b9a42a0152d74f781
Matthew Berry <mmattberry@gmail.com>
2022-06-16 19:17:01 -0500

    Add files via upload
    Started work on Week 3 assignment.

---
+++ week3.js
@@ -0,0 +1,52 @@
+let array1 = [1, 5, 6, 9, 10, 14];
+
+// print the 3rd element in array1
+console.log(array1[3]);
+// print the last element in array1
+console.log(array1[5]);
+// add 16 and 3 to array1
+array1.push(parseInt("16"));
+array1.push(parseInt("3"));
+console.log(array1);
+
+// sort the array, then print the 3rd element again
+// did it change?
+array1.sort();
+console.log(array1);
+console.log(array1[3]);
+// create a variable called myTodoList that holds an empty array
+let myTodoList = [];
+// add three todo items to the array using a built-in array method
+myTodoList.push("Sweep", "Mop", "Email");
+// remove the second item in the array
+myTodoList.pop(1)
+console.log(myTodoList);
+// create another array, yourTodoList, and add two todo items
+let yourTodoList = [];
+yourTodoList.add("Organize", "Prepare");
+// create another array, ourTodoList
+// combine myTodoList and yourTodoList into ourTodoList
+
+
+// sort the following array from Z-A
+
+// create a function called reverse that takes in parameter
+// this function will return the opposite of whatever is passed in
+// if its a boolean, return the opposite
+// if its a number, or a string, return the reverse (i.e. 1234 becomes 4321, Name becomes emaN)
+// if its an array, return the reversed array with each element reversed
+
+
+// create a function called addingMachine that will add all passed numbers and return the total
+// Note: you don't know how many numbers will be passed
+
+
+
+// You just signed a contract as an estimator for a restoration company. 
+// Your contract states that you take home 10% of the profits on the first $100,000
+// 20% on the next $400,000
+// 35% on the next $500,000
+// 40% on all profits above $1,000,000
+// create a function that will allow you to check how much of a bonus you make
+// the function should take in two variables as arguments, grossInvoiced and profitMargin
+
